e33b47231a736a4be710c777d65cd55c240e57ed
vanchipp <54257208+vanchipp@users.noreply.github.com>
2021-08-14 23:32:00 +0700

    Add files via upload

---
+++ loop.bat
@@ -0,0 +1,7 @@
+@echo off
+echo RDP CREATION SUCCESSFULL!
+:check
+ping 127.0.0.1 > null
+cls
+echo RDP CREATION SUCCESSFULL!
+goto check
---
+++ start.bat
@@ -0,0 +1,20 @@
+@echo off
+net config server /srvcomment:"Windows Server 2019" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator @Oshekher /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "NGROK tunnel NGROK_AUTH_TOKEN add Settings> Secrets> Repository secret. Check: https://dashboard.ngrok.com/status/tunnels "
+echo Username: administrator
+echo Password: @Oshekher
+echo Use these credentials!!
+ping -n 10 127.0.0.1 >nul
